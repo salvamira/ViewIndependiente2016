77ea99686ea5cccd92174cbd64051607aae3d14c
Salva Mira <smira@iesperemaria.com>
2016-09-29 10:47:32 +0200

    Initial commit

---
+++ app/src/main/java/net/somtic/viewindependiente2016/MyView.java
@@ -0,0 +1,34 @@
+package net.somtic.viewindependiente2016;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Salva on 29/09/2016.
+ */
+
+public class MyView extends View {
+    private ShapeDrawable figura;
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        figura = new ShapeDrawable(new OvalShape());
+        figura.getPaint().setColor(0xff0000ff);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        figura.setBounds(0, 0, w, h);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        figura.draw(canvas);
+    }
+}
